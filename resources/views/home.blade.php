3b3fcf2135198fba17275192162149658e2535de
mutou_manami <yori949dmq@kir.biglobe.ne.jp>
2020-11-01 01:00:48 +0900

    #773 [ヘッダーの設定] の作成方法を調べる

---
+++ resources/views/home.blade.php
@@ -0,0 +1,18 @@
+{{-- layouts/admin.blade.phpを読み込む --}}
+@extends('layouts.admin')
+{{-- admin.blade.phpの@yield('title')に'ホーム画面'を埋め込む --}}
+@section('title', 'ホーム画面')
+
+{{-- admin.blade.phpの@yield('content')に以下のタグを埋め込む --}}
+@section('content')
+    <div class="container">
+        <div class="row">
+            <div class="col-md-8 mx-auto">
+                <h2>マイページ</h2>
+            </div>
+        </div>
+    </div>
+@endsection
+
+
+
