bd71321d43678f9c1bddf97b50c569bbae59b57b
Mutou manami <yori949dmq@kir.biglobe.ne.jp>
2020-11-23 11:33:42 +0000

    プロフィール設定画面変更

--- resources/views/setting/create.blade.php
+++ resources/views/setting/create.blade.php
@@ -6,7 +6,7 @@
     <div class="container">
         <div class="row">
             <div class="col-md-8 mx-auto">
-                <h2>Myプロフィール</h2>
+                <h2>プロフィール</h2>
                 <form action="{{ action('Admin\SettingController@create') }}" method="post" enctype="multipart/form-data">
 
                     @if (count($errors) > 0)
@@ -17,28 +17,43 @@
                         </ul>
                     @endif
                     <div class="form-group row">
-                        <label class="col-md-2">氏名(name)</label>
+                        <label class="col-md-2">ニックネーム(nickname)</label>
                         <div class="col-md-10">
                             <input type="text" class="form-control" name="name" value="{{ old('name') }}">
                         </div>
                     </div>
+                     {{--  性別選択欄　ラジオボタン  --}}
                     <div class="form-group row">
                         <label class="col-md-2">性別(gender)</label>
-                        <div class="col-md-10">
-                            <input type="text" class="form-control" name="gender" value="{{ old('gender') }}">
+                        <div class="col-sm-10">
+                            　<input type="radio" name="gender" checked="checked" value="man">男性
+                            　<input type="radio" name="gender" value="woman">女性
+                            　<input type="radio" name="gender" value="neither">中性
                         </div>
                     </div>
+                     {{--  年代選択設定　ドロップダウン  --}}
                     <div class="form-group row">
-                        <label class="col-md-2">趣味(hobby)</label>
-                        <div class="col-md-10">
-                            <input type="text" class="form-control" name="hobby" value="{{ old('hobby') }}">
+                        <label class="col-md-2">年齢(age)</label>
+                        <div class="col-sm-10">
+                            　<select name="age">
+                               <option value="">選択してください</option>
+                               <option value="20歳未満">20歳未満</option>
+                               <option value="20-29歳">20-29歳</option>
+                               <option value="30-39歳">30-39歳</option>
+                               <option value="40-49歳">40-49歳</option>
+                               <option value="50-59歳">50-59歳</option>
+                               <option value="60-69歳">60-69歳</option>
+                               <option value="70-79歳">70-79歳</option>
+                               <option value="80歳以上">80歳以上</option>
+                              </select>
                         </div>
                     </div>
-                    
+                     {{--  目標選択　ラジオボタン  --}}
                     <div class="form-group row">
-                        <label class="col-md-2">自己紹介欄(introduction)</label>
+                        <label class="col-md-2">目標(aim)</label>
                         <div class="col-md-10">
-                            <textarea class="form-control" name="introduction" rows="20">{{ old('introduction') }}</textarea>
+                            <input type="radio" name="aim" value="1" checked="checked">健康維持
+                            <input type="radio" name="aim" value="2">ダイエット・身体づくり
                         </div>
                     </div>
                     {{ csrf_field() }}
